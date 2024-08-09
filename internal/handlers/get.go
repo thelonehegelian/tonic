74e321a52b92d382c461eb2b6ae6b0ecdfc4a7ab
lonehegelian <thelonehegelian@protonmail.com>
2024-08-09 23:42:43 +0100

    create a separate ParseRequest function
    this will be used for filtering incoming requests

--- internal/handlers/get.go
+++ internal/handlers/get.go
@@ -21,3 +21,19 @@ func ParseGetRequest(req string) *Request {
 	}
 	return request
 }
+
+func ParseRequest(req string) *Request {
+	lines := strings.Split(req, "\r\n")
+	requestLine := lines[0]
+	parts := strings.Fields(requestLine)
+	headers := ParseHeaders(lines[1:])
+
+	request := &Request{
+		Method:  parts[0],
+		URI:     parts[1],
+		Version: parts[2],
+		Headers: headers,
+		Body:    sampleBody,
+	}
+	return request
+}
