61db14ad4292736296969841244ce1b3011e0fa8
lonehegelian <thelonehegelian@protonmail.com>
2024-08-09 23:43:51 +0100

    create a Router function
    this is the interface to allow users to create routes efficiently

--- internal/handlers/request.go
+++ internal/handlers/request.go
@@ -10,12 +10,47 @@ type Request struct {
 	Body    string
 }
 
+
+// ? what should the handler function take
+// the handler function should have a context which would have all the information about the request
+// the handler function should return a response
+type HandlerFunc func() Response
+
+type Route struct {
+	Method  string
+	Path    string
+	Handler HandlerFunc
+}
+
+type Router struct {
+	Routes []Route
+}
+
+func NewRouter() *Router {
+	return &Router{}
+}
+
+func (r *Router) GET(path string, handler HandlerFunc) {
+	// create a Route
+	route := Route{
+		Method:  "GET",
+		Path:    path,
+		Handler: handler,
+	}
+
+	r.Routes = append(r.Routes, route)
+}
+
+// router := NewRouter()
+
+// router("/phones", "GET", PhonesHandler)
+// on hitting the /phones route call the handlerFunction, if the function does not exist give error
+
 func GetRequestMethod(req string) string {
 	return strings.Split(req, " ")[0]
 }
 
 func ParseHeaders(lines []string) map[string]string {
-
 	headers := make(map[string]string)
 	for _, line := range lines[1:] {
 		// if we hit an empty line, headers are done
