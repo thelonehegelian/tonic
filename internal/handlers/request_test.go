5de3cf4bac819ee78b46d083167c159f5a8e93f5
lonehegelian <thelonehegelian@protonmail.com>
2024-08-24 19:17:03 +0100

    add tests for parseHeaders

--- internal/handlers/request_test.go
+++ internal/handlers/request_test.go
@@ -1,34 +1,85 @@
 package handlers
 
 import (
-	"fmt"
-	"log"
 	"reflect"
 	"testing"
 )
 
 func TestParseHeaders(t *testing.T) {
-	input := []string{
-		"GET / HTTP/1.1",
-		"Host: localhost:8080",
-		"User-Agent: curl/7.64.1",
-		"Accept: */*",
-		"",
+	tests := []struct {
+		name     string
+		input    []string
+		expected map[string]string
+		wantErr  bool
+	}{
+		{
+			name: "Valid headers with GET request",
+			input: []string{
+				"GET / HTTP/1.1",
+				"Host: localhost:8080",
+				"User-Agent: curl/7.64.1",
+				"Accept: */*",
+				"",
+			},
+			expected: map[string]string{
+				"Method":     "GET",
+				"Path":       "/",
+				"Version":    "HTTP/1.1",
+				"Host":       "localhost:8080",
+				"User-Agent": "curl/7.64.1",
+				"Accept":     "*/*",
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "Method not allowed",
+			input: []string{
+				"BET / HTTP/1.1",
+				"Host: localhost:8080",
+				"User-Agent: curl/7.64.1",
+				"Accept: */*",
+				"",
+			},
+			expected: nil,
+			wantErr:  true,
+		},
+
+		{
+			name: "Path is not Valid",
+			input: []string{
+				"GET path HTTP/1.1",
+				"Host: localhost:8080",
+				"User-Agent: curl/7.64.1",
+				"Accept: */*",
+				"",
+			},
+			expected: nil,
+			wantErr:  true,
+		},
+		{
+			name: "HTTP Version not Allowed",
+			input: []string{
+				"GET / HTTP/2.2",
+				"Host: localhost:8080",
+				"User-Agent: curl/7.64.1",
+				"Accept: */*",
+				"",
+			},
+			expected: nil,
+			wantErr:  true,
+		},
 	}
 
-	expected := map[string]string{
-		"Method":     "GET",
-		"Path":       "/",
-		"Version":    "HTTP/1.1",
-		"Host":       "localhost:8080",
-		"User-Agent": "curl/7.64.1",
-		"Accept":     "*/*",
-	}
-
-	result := ParseHeaders(input)
-
-	log.Println(fmt.Sprintf("result: %v", result))
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("ParseHeaders(%v) = %v; want %v", input, result, expected)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := ParseHeaders(test.input)
+			if (err != nil) != test.wantErr {
+				t.Errorf("ParseHeaders() error = %v, wantErr %v", err, test.wantErr)
+			}
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("ParseHeaders() = %v, want %v", result, test.expected)
+			}
+		})
 	}
 }
